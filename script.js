7dca6c90f125a75676a56fcaad53f03d3f3bceaf
Francesco <chiccocarone@gmail.com>
2023-10-02 16:44:12 +0200

    Update

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const cittaInput = document.querySelector('input[name="citta"]');
-    const inputContainers = document.querySelectorAll('.input-container:not(.cittainput)');
 
-    cittaInput.addEventListener('input', function () {
-      if (cittaInput.value.trim() === '') {
-        // Hide all input containers except for "Città"
-        inputContainers.forEach(function (container) {
-          container.classList.add('hidden');
+    const cityDropdown = document.getElementById("citta");
+
+    // Chiamata AJAX per ottenere l'elenco dei file CSV nella cartella
+    fetch('database_citta')
+        .then(response => response.json())
+        .then(data => {
+            data.files.forEach(file => {
+                // Estrai il nome della città dal nome del file
+                const cityName = file.replace('.csv', '');
+
+                // Aggiungi un'opzione al menu dropdown
+                const option = document.createElement("option");
+                option.value = cityName;
+                option.textContent = cityName;
+                cityDropdown.appendChild(option);
+            });
+        })
+        .catch(error => {
+            console.error("Errore durante il recupero dell'elenco dei file CSV:", error);
         });
-      } else {
-        // Show all input containers
-        inputContainers.forEach(function (container) {
-          container.classList.remove('hidden');
-        });
-      }
-    });
   });
