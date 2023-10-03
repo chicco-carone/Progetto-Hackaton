9904a37ed0c9497623d17af5e596d876663a5009
Francesco <chiccocarone@gmail.com>
2023-10-03 16:35:13 +0200

    final update

--- script-js/searchdatabase.js
+++ script-js/searchdatabase.js
@@ -11,6 +11,7 @@ $(document).ready(function() {
         let cap = document.getElementById("cap");
 
         var selectedFile = comune.options[comune.selectedIndex].text;
+        openPopup();
 
         // Eseguire la richiesta AJAX per caricare il file JSON selezionato
         $.getJSON("script_per_database/output_comuni/"+ selectedFile + ".json", function(data) {
@@ -24,11 +25,43 @@ $(document).ready(function() {
 
             if (risultato) {
                 console.log("Elemento trovato:", risultato);
+                openPopup(true, risultato.Denominazione, risultato.Via, risultato.CIS, risultato.CAP, risultato.Civico);
             // Fai qualcosa con l'elemento trovato
             } else {
+                openPopup(false)
                 console.log("Elemento non trovato");
             }
         });
 
     });
 });
+
+function closePopup() {
+    var popup = document.getElementById("popup");
+    popup.style.display = "none";
+}
+
+function openPopup(trovato, nomeLocale, indirizzo, cis, cap, ncivico){
+    var popup = document.getElementById("popup");
+    var popupContent = document.getElementById("popup-content");
+
+    if (trovato){
+        var content = "Elemento trovato!<br>Il nome dell'alloggio è: " + nomeLocale + "<br>Si trova in via: " + indirizzo + " con numero civico: " + ncivico + "<br>Il cis del locale è: " + cis;
+        content = content.replace(/\n/g, "<br>");
+        popupContent.innerHTML = content; 
+        var tickImage = document.createElement("img");
+        tickImage.src = "images/si.png";
+        popupContent.appendChild(tickImage);
+    }else {
+        var content = "Elemento non trovato!!\nl'alloggio inserito non è stato trovato nel database, potrebbe essere un alloggio abusivo. Si prega di verificare.\n(ultima data di aggiornamento del database: 03/09/2023)";
+        content = content.replace(/\n/g, "<br>");
+        popupContent.innerHTML = content;
+        var tickImage = document.createElement("img");
+        tickImage.src = "images/no.png";
+        popupContent.appendChild(tickImage);
+    }
+
+    
+    // Show the popup
+    popup.style.display = "block";
+}
